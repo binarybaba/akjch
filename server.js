7aa4387260cafc64c82b0d5c928dc6f3bbcfd52d
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-13 15:16:13 +0530

    add dashboard routes

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var session = require('express-session');
+var passport = require('passport');
+var twitterStrategy = require('passport-twitter').Strategy;
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var xlsx = require('xlsx');
 var workbook = xlsx.readFile('./stocks2.xlsx');
 var sheet = workbook.SheetNames[0];
 var worksheet = workbook.Sheets[sheet];
-console.log(worksheet);
-
-
-
+//console.log(worksheet);
+var dashboardRouter = require('./controllers/routes/dashboardRouter');
+// TODO: add twitter oAuth, on success, store user in mongo and redirect to dashboard.ejs
+//TODO: require xlsx in dashboard controller, load and wire up angular to filter to select the list of portfolios
+//TODO: and post to dashboard to render highstock
 
 /*console.log(workbook);*/
 
@@ -16,10 +22,18 @@ console.log(worksheet);
 app.use(express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-    /*res.render('index');*/
-    res.json(worksheet)
+    res.render('pages/index');
+    /*res.json(worksheet) <- testing*/
 });
+
+
+
+
+/*Setting up routes*/
+app.use('/dashboard', dashboardRouter);
+
 
 app.listen(PORT, function () {
     console.log('Listening at port ' + PORT);
