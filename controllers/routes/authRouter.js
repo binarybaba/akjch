0cca30b7d654d075f0589c0a6f2d5d58557572b5
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-14 14:17:04 +0530

    wiring up signup authRouter

--- controllers/routes/authRouter.js
+++ controllers/routes/authRouter.js
@@ -7,7 +7,7 @@ var mongodb = require('mongodb').MongoClient;
     authRouter.route('/signup')
         .post(function(req, res){
             /*bodypraser will transform req into cool json*/
-            console.log(req.body);
+            console.log(req);
             res.send('cool');
 
         })
