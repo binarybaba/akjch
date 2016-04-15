a0ed7d873ea99a013d0bacde776459e86fc46551
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-15 17:31:26 +0530

    add logout route to kick user out of session

--- controllers/routes/dashboardRouter.js
+++ controllers/routes/dashboardRouter.js
@@ -1,19 +1,28 @@
 var express = require('express');
 var dashboardRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var passport = require('passport');
 
 
 dashboardRouter.route('/')
     .get(function(req, res){
         if(req.user) {
             res.render('pages/dashboard', {
-                name: req.user.username
+                name: req.user.name,
+                user:req.user.username,
+                id:req.user._id
             })
         }
         else{
             res.redirect('/');
         }
     });
+dashboardRouter.route('/logout')
+    .get(function(req, res){
+        req.logOut();
+        res.redirect('/');
+    });
+
 
 
 
