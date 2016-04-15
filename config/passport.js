34140d59cfd465acd50588b84f645d2250d905e0
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-15 15:00:11 +0530

    wiring up serialize and deserialize

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ module.exports = function(app){
     app.use(passport.session());
     
     passport.serializeUser(function(user, done){ //bundles up the user into the session to store for later. used to trim that down
-        done(null,user); //done(err,result) ignoring the err for now
+        done(null,user); //done(err,result) ignoring the err for now 
     })
     passport.deserializeUser(function(user, done){
         done(null, user);
