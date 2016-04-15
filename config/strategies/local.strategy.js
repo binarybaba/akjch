1c825fe6058ff51a09e99128387e6deca434edc1
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-15 15:01:16 +0530

    local strategy no authentication for now

--- config/strategies/local.strategy.js
+++ config/strategies/local.strategy.js
@@ -5,7 +5,7 @@ module.exports = function(){
         usernameField:'uname', // id/name of the field in the view
         passwordField:'pass' //id/name of the field in the view
     }, function(username, password, done){
-        //this function determines validation. goto dbase and check username and stuff
+        //this function determines validation. goto dbase and check username and stuff 
         var user = {
             username:username,
             password:password
