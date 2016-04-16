7727bfb2ebeafc9291899f8a103bede30a2519a0
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-16 22:59:10 +0530

    read stock list from model rather than excel sheet

--- controllers/routes/dashboardRouter.js
+++ controllers/routes/dashboardRouter.js
@@ -2,6 +2,8 @@ var express = require('express');
 var dashboardRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var passport = require('passport');
+var stockList = require('../../model/stocklist.js');
+
 
 
 dashboardRouter.route('/')
@@ -23,16 +25,22 @@ dashboardRouter.route('/logout')
         res.redirect('/');
     });
 
-dashboardRouter.route('/portfolio')
+dashboardRouter.route('/portfolios')
     .get(function(req, res){
-        if(!req.user){
-            res.json(req.user.portfolio);
-        }
+        if(req.user){
+            /*res.json(req.user.portfolio);*/
+            res.json(stockList);
+     }
         else{
             res.redirect('/');
         }
+    });
+dashboardRouter.route('/user/portfolios')
+    .get(function(req, res){
+        if(req.user){
+            res.send('users portfolios');
+        }
     })
-
     /*TODO: Add post route and update to model*/
 
 
