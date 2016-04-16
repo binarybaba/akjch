46f5b035562060dbb7b5cc27b8aa720666d58642
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-16 17:10:37 +0530

    load worksheet from disk and convert to cool js object

---
+++ config/worksheet/load.js
@@ -0,0 +1,23 @@
+module.exports = function(){
+    var xlsx = require('xlsx'),
+        workbook = xlsx.readFile('./stocks2.xlsx'),
+        sheet = workbook.SheetNames[0],
+        worksheet = workbook.Sheets[sheet],
+        ref = '!ref',
+        cols = worksheet[ref].split(':'),
+        nums = cols.toString().match(/(\d+)/g),
+        newSheet =[];
+    len = nums[1]-nums[0];
+    for(var i = 2; i<len;i++){
+        newSheet.push({
+            "name":worksheet["B"+i].v,
+            "ticker":worksheet["A"+i].v,
+            "weight":Math.random() * (100 -10) + 10 //random weight
+        });
+    }
+    console.log(newSheet);
+    //TODO: require model/stocks/load(newsheet)
+}
+
+
+
