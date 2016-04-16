3e5ca9cb3602e7cb10b18e4d84469ef1ccd148f3
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-16 23:09:34 +0530

    refactor

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser'); //passport will need this to store the cookie
-var session = require('express-session');
-var passport = require('passport');
-require('./config/worksheet/load.js')();
+var express = require('express'),
+    app = express(),
+    PORT = process.env.PORT || 3000,
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'), //passport will need this to store the cookie
+    session = require('express-session'),
+    passport = require('passport');
+/*Setup the stocks in model*/
+require('./config/stockdb.js')();
 
-
-
-var dashboardRouter = require('./controllers/routes/dashboardRouter');
-var authRouter = require('./controllers/routes/authRouter');
-
+/*Requiring Custom Routes*/
+var dashboardRouter = require('./controllers/routes/dashboardRouter'),
+    authRouter = require('./controllers/routes/authRouter');
 
 /*TODO: In Dashboard get the list of user's portfolios. if !portfolios then ask to make portfolio by sending list of portfolios from db or xlsx (omg deal with weights too). */
-/*TODO: try require xlsx and load data to mongo master portfolio for better access rather than reading xlsx file every time*/
 /*TODO: Wire up angular to deal with the filter and let user add the list to his custom portfolio -> save -> express -> model -> mongo*/
 /*TODO: compare portfolio button -> send to Highstock. Figure out a way to add a directive to angular or do it vanilla if there's no time*/
 
-
-/*console.log(workbook);*/
-
-/*Middlewares*/
+/*Wiring up middlewares*/
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'true'}));
@@ -35,22 +30,17 @@ app.use(session({
 /*Refactoring all passport stuff and pulling it in from the config*/
 require('./config/passport')(app);
 
-
+/*Setting views and engine*/
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.render('pages/index');
-    /*res.json(worksheet) <- testing*/
 });
 
-
-
-/*Setting up routes*/
+/*Setting up custom routes*/
 app.use('/dashboard', dashboardRouter);
 app.use('/auth', authRouter);
-
-
 
 app.listen(PORT, function () {
     console.log('Listening at port ' + PORT);
