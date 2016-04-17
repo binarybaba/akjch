72d135df14443c43c9306231ae941c6f127f39cf
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-17 19:12:38 +0530

    pull stock list from model no require

--- controllers/routes/dashboardRouter.js
+++ controllers/routes/dashboardRouter.js
@@ -2,7 +2,8 @@ var express = require('express');
 var dashboardRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var passport = require('passport');
-var stockList = require('../../model/stocklist.js');
+var util = require('util');
+/*var stockList = require('../../model/stocklist.js');*/
 
 
 
@@ -26,15 +27,29 @@ dashboardRouter.route('/logout')
     });
 
 dashboardRouter.route('/stocklist')
-    .get(function(req, res){
+    .get(function(req, res,next){
+        var stocks=[];
         if(req.user){
+            var url = 'mongodb://localhost:27017/akjch';
+            mongodb.connect(url, function(err, db){
+                var cursor = db.collection('stocks').find();
+                cursor.forEach(function(doc, err){
+                    stocks.push(doc);
+                 },function(){
+                    db.close();
+                    res.send(stocks);
+                });
+
+            });
+
             /*res.json(req.user.portfolio);*/
-            res.json(stockList);
+            /*res.send(stockList);*/
      }
         else{
             res.redirect('/');
         }
     });
+
 dashboardRouter.route('/user/portfolios')
     .get(function(req, res){
         if(req.user){
