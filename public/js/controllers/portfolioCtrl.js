44c84219844c58ae008e53c77b168368ea10fa17
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-18 02:14:26 +0530

    add update/remove/add portfolios no persist

--- public/js/controllers/portfolioCtrl.js
+++ public/js/controllers/portfolioCtrl.js
@@ -1,6 +1,22 @@
 angular.module('akjch')
-    .controller('portfoliosCtrl', ['$scope', 'stockFactory', function($scope, stockFactory){
-
+    .controller('portfoliosCtrl', ['$scope', 'stockFactory', '$http', function($scope, stockFactory, $http){
+        function findPortfolio(portf){
+            var filtered = $scope.tempPortfolios.filter(function(val){
+                return val.name === portf;
+            });
+            return filtered;
+        }
+        function findStock(stock){
+            var results = [];
+            $scope.tempPortfolios.filter(function(p){
+                if(p.name === $scope.portfolioName){
+                    results = p.stocks.filter(function(s){
+                        return s === stock;
+                    });
+                }
+            });
+            return results;
+        }
 
         /*Populating view with stock list from db*/
         stockFactory.getStockList()
@@ -8,130 +24,38 @@ angular.module('akjch')
                 $scope.MasterStocks = data;
         });
 
-        //pulled in from user's database Wire up service to send the list.
+        //Placeholder for temporary portfolios
         $scope.tempPortfolios= [
-            {
+           /* {
                 "name":"dummyPortfolios",
                 "stocks":['ABC','NASDAQ']
-            }
+            }*/
         ];
 
-        //list of all portfolios pulled in from service.
-         /*   $scope.MasterStocks = [
-            {
-                "ticker":"SPYV",
-                "name":"S&P 500"
-            },
-            {
-                "ticker":"DDD",
-                "name":"3D Systems Corporation"
-            },
-            {
-                "ticker":"MMM",
-                "name":"3M Company"
-            },
-            {
-                "ticker":"WBAI",
-                "name":"500.com Limited"
-            },
-            {
-                "ticker":"WUBA",
-                "name":"500.com Limited"
-            },
-            {
-                "ticker":"AHC",
-                "name":"A.H Belo Corporation"
-            },
-            {
-                "ticker":"ATEN",
-                "name":"A10 Networks"
-            },
-            {
-                "ticker":"AAC",
-                "name":"AAc Holdings"
-            },
-            {
-                "ticker":"AIR",
-                "name":"AAR Corp"
-            },
-            {
-                "ticker":"AAN",
-                "name":"Aaron Corp"
-            },
-            {
-                "ticker":"ABB",
-                "name":"ABB Ltd"
-            }
-];*/
+        $scope.addPortfolio = function(stock,portfolio){
+            if($scope.portfolioName){ //cheap validation
+                var pf = findPortfolio(portfolio);
+                var sf = findStock(stock.ticker);
 
-        function findPortfolio(){
-              var filtered = $scope.tempPortfolios.filter(function(val){
-                  return val.name == $scope.portfolioName;
-              });
-             return filtered;
-         }
-
-        $scope.addP = function(stock,portfolio){
-            //TODO: if portfolio exists { 
-            //TODO:      if stock !exist { 
-            //TODO:             push stock.ticker
-            //TODO:            }else {
-            //TODO:                  do nothing
-            //TODO:            } 
-            //TODO:    else {
-            //TODO:          add new portfolio with stock
-            //TODO:         }
-            
-            console.log(stock.ticker);
-            console.log(portfolio);
-        }
-
-
-
-        $scope.addPortfolio = function(stock){
-             var pf = findPortfolio();
-            //if the portfolio name is already present ie if the user is updating a portfolio
-
-            if(pf.length != 0){
-                $scope.tempPortfolios.map(function(elem){
-                    if(elem.name == $scope.portfolioName){
-                        elem.stocks.push(stock.ticker);
+                if(pf.length != 0){ //if portfolio exists
+                    if(sf.length===0){ // if stock not present, add it
+                        $scope.tempPortfolios.map(function(p){
+                            if(p.name === portfolio){
+                                p.stocks.push(stock.ticker);
+                            }
+                        });
                     }
-                })
-            }
-            else {
-                 var newPortfolio = {
-                     "name":$scope.portfolioName,
-                     "stocks":[]
-                 }
-                newPortfolio.stocks.push(stock.ticker);
-                $scope.tempPortfolios.push(newPortfolio);
-             }
-
-
-
-
-
-
-
-
-            /*var newPortfolio = {
-                "name":$scope.portfolioName,
-                "stocks":[]
-            }
-            newPortfolio.stocks.push(stock);*/
-
-            /*$scope.tempPortfolios.name=$scope.name;
-            $scope.tempPortfolios*/
-            /*$scope.tempPortfolios.map(function(portfolio){
-                //if user is udating already present portfolio
-                if(portfolio.name.toLowerCase() == $scope.portfolioName.toLowerCase()){
-                    portfolio.stocks.push(stock);
                 }
-            });*/
-            
-            /*$scope.tempPortfolios.push(newPortfolio);*/
+                else{ //make a new portfolio and add the stock
+                    var newPortfolio = {
+                        "name":portfolio,
+                        "stocks":[]
+                    };
+                    newPortfolio.stocks.push(stock.ticker);
+                    $scope.tempPortfolios.push(newPortfolio);
 
+                }
+            }
         };
 
         $scope.removeStock = function(portfolio,stock){
@@ -146,6 +70,7 @@ angular.module('akjch')
                 }
             })
         };
+
         $scope.removePortfolio = function(name){
             $scope.tempPortfolios.map(function(elem){
                 if(elem.name === name){
@@ -154,5 +79,16 @@ angular.module('akjch')
                 }
             })
         };
+        //TODO : make update link to post tempPortfolios to service->model  then reset tempPortfolios immediately
+        $scope.updatePortfolios = function(){
+            $http({
+                url:'/dashboard/user/portfolios',
+                method:'POST',
+                data:$scope.tempPortfolios
+            }).then(function(response){
+                console.log(response);
+                $scope.tempPortfolios=[];
+            })
 
+        }
     }]);
