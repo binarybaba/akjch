f65ae8c17180c5919a137bf77e7e6d4440458210
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-17 19:13:20 +0530

    wiring to get stock list from model

--- model/stocklist.js
+++ model/stocklist.js
@@ -1,20 +1,28 @@
 var mongodb = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/akjch';
-var Stocks;
-mongodb.connect(url, function(err, db){
-        db.collection('stocks').find().toArray(function(err, items){
-            console.log('ITEMS===================');
-            console.log(items);
-            console.log('ITEMS===================');
-            console.log(typeof items);
-            module.exports = items;
-            //TODO: items is an object and angular is expecting an array. FIX THIS
-        })
+var util = require('util');
+module.exports = function(){
+var res=[];
+    mongodb.connect(url, function(err, db){
 
+        var cursor = db.collection('stocks').find();
+        cursor.forEach(function(doc, err){
+            res.push(doc);
+        });
+
+        /*db.collection('stocks').find().toArray(function(err, items){
+         module.exports = items;
+         /!*console.log('Logging from stocklist.js. Items from db is-'+ util.inspect(items,false,null));*!/
+
+         //TODO: items is an object and angular is expecting an array. FIX THIS
+         })
+         */
     });
+return res;
+};
 
 
-
+/*
 
 
 var stocklist = [
@@ -37,4 +45,5 @@ var stocklist = [
 },
 
 ];
+*/
 
