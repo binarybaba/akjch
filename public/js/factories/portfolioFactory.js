8efe104e5b14281e2239558d9a092b1434fb8ca9
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-18 15:13:36 +0530

    portfolio factory to get/put/delete portfolios to server

---
+++ public/js/factories/portfolioFactory.js
@@ -0,0 +1,45 @@
+angular.module('akjch')
+    .factory('portfolioFactory', ['$http', function($http){
+        var url = '/dashboard/user/portfolios'
+        var portfolioSvcs = {
+            getPortfolioList: function(){
+                var promise = $http({
+                    url: url,
+                    method: 'GET',
+                }).then(function success(response){
+                    console.log(response);
+                    return response.data;
+                }, function err(response){
+                    return response.data;
+                });
+                return promise;
+            },
+            postPortfolios: function(portfolios){
+                var promise = $http({
+                    url: url,
+                    method: 'POST',
+                    data:portfolios
+                }).then(function success(response){
+                    return response.data;
+                }, function err(response){
+                    return response.data;
+                });
+                return promise;
+            },
+            deletePortfolio: function (portfolio){
+                var promise = $http({
+                    url: url,
+                    method:'PUT',
+                    data:{"name":portfolio}
+                }). then(function success(response){
+                    console.log('got this data for delete from server '+response.data);
+                    return response.data;
+                }, function err(response){
+                    return response.data;
+                });
+                return promise;
+            }
+        }
+
+        return portfolioSvcs;
+    }])
