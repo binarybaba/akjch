7cb37cef147b19ae7652325b3ee394daf6679ebf
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-04-20 17:23:09 +0530

    serialized and deserialized user

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-
+var util = require('util');
 var passport = require('passport');
 
 
@@ -7,9 +7,13 @@ module.exports = function(app){
     app.use(passport.session());
     
     passport.serializeUser(function(user, done){ //bundles up the user into the session to store for later. used to trim that down
-        done(null,user); //done(err,result) ignoring the err for now 
+        done(null,user); //done(err,result) ignoring the err for now
+        console.log('serialize user');
+        console.log(util.inspect(user, false, null));
     })
     passport.deserializeUser(function(user, done){
+        console.log('deserialize user');
+        console.log(util.inspect(user, false, null));
         done(null, user);
 
     })
